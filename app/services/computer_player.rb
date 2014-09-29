197cb51df61a030b1cd792f089740caab7aa7d58
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2014-09-29 12:17:02 -0700

    Computer player with step moves

--- app/services/computer_player.rb
+++ app/services/computer_player.rb
@@ -1,5 +1,5 @@
 class ComputerPlayer
-  attr_accessor :chips, :black, :white
+  attr_accessor :chips
   BLACK_MOVES = [10, 20, 30, 40, 50, 60, 
                  11, 21, 31, 41, 51, 61, 
                  12, 22, 32, 42, 52, 62,
@@ -19,14 +19,12 @@ class ComputerPlayer
 
   def initialize(params)
     @chips = params[:chips] || {black: [], white: []}
-    @black = @chips[:black] || []
-    @white = @chips[:white] || []
     @search_depth = params[:search_depth] || 5 #TODO Tinker with default
     @color = params[:color] || :black
   end
 
   def move(color)
-    if @black.length >= 10
+    if @chips[:black].length >= 10
       choose_step_move(color)
     else
       choose_move(color, -1, 1)
@@ -48,18 +46,20 @@ class ComputerPlayer
       my_move.score = beta
     end
     legal_moves = all_legal_moves(color)
-    my_move.added_chip = legal_moves.sample
+    my_move = legal_moves.sample
 
     legal_moves.each do |move|
-      @chips[color] << move
+      @chips[color] << move.added_chip
+      @chips[color].delete(move.deleted_chip) if move.deleted_chip
       reply = choose_move(other_color, alpha, beta, depth - 1)
-      @chips[color].delete(move)
+      @chips[color].delete(move.added_chip)
+      @chips[color] << move.deleted_chip if move.deleted_chip
       if color == @color && reply.score > my_move.score  
-        my_move = m
+        my_move = move
         my_move.score = reply.score
         alpha = reply.score
       elsif color == other_color && replay.score > my_move.score
-        my_move.move = m
+        my_move = move
         my_move.score = reply.score
         beta = reply.score
       end
@@ -73,26 +73,47 @@ class ComputerPlayer
   def evaluate_board
   end
 
-  def all_legal_moves(color)
-    connected_points = get_connected_points(color)
-    if color == :black
-      legal_moves = BLACK_MOVES - connected_points - @black - @white
-    elsif color == :white
-      legal_moves = WHITE_MOVES - connected_points - @white - @white
+  def all_legal_moves(color) 
+    legal_moves = []
+    if @chips[:black].length >= 10
+      0..9.each do |n|
+        deleted_point = @chips[color].delete_at(n)
+        legal_points = get_legal_points(color)
+        legal_points.each do |point|
+          legal_moves.push(Move.new(color, point, deleted_point)) unless point == deleted_point
+        end
+        @chips[color].insert(n, deleted_point)
+      end
     else
-      #TODO handle error
+      legal_points = get_legal_points(color)
+      legal_points.each do |point|
+        legal_moves.push(Move.new(color, point))
+      end
     end
     return legal_moves
   end
+
+  def get_legal_points(color)
+    connected_points = get_connected_points(color)
+    if color == :black
+      legal_points = BLACK_MOVES - connected_points - @chips[:black] - @chips[:white]
+    elsif color == :white
+      legal_points = WHITE_MOVES - connected_points - @chips[:white] - @chips[:white]
+    else
+      #TODO handle error
+    end
+    legal_points
+  end
+
 
   def get_connected_points(color)
     connected_points = []
     test_points = []
     if color == :black
-      own_chips = @black
+      own_chips = @chips[:black]
       total_moves = BLACK_MOVES
     elsif color == :white
-      own_chips = @black
+      own_chips = @chips[:black]
       total_moves = WHITE_MOVES
     else
       #TODO handle color error
@@ -104,7 +125,7 @@ class ComputerPlayer
         for j in -1..1
           point = (chipx + i)*10 + (chipy + j)
           if (BLACK_MOVES.include?(point) && test_points.exclude?(point) && 
-              @white.exclude?(point) && @black.exclude?(point))
+              @chips[:white].exclude?(point) && @chips[:black].exclude?(point))
             test_points << point
           end
         end
@@ -131,9 +152,9 @@ class ComputerPlayer
 
   def connected_chips(color, point)
     if color == :black
-      colored_chips = @black
+      colored_chips = @chips[:black]
     elsif color == :white
-      colored_chips = @white
+      colored_chips = @chips[:white]
     else
       #TODO handle color error
     end
@@ -153,42 +174,3 @@ class ComputerPlayer
     return connected_chips
   end
 end
-        
-  
-      
-
-=begin
-  This is the pseudo code for alpha beta pruning
-
-  public Best chooseMove(boolean side,
-    int alpha, int beta) {
-    Best myBest = new Best(); // My best move
-    Best reply; // Opponent’s best reply
-
-    if ("this" Grid is full or has a win) {
-      return a Best with the Grid’s score, no move;
-    }
-    if (side == COMPUTER) {
-      myBest.score = alpha;
-    } else {
-      myBest.score = beta;
-    }
-    myBest.move = any legal move;
-    for (each legal move m) {
-      perform move m; // Modifies "this" Grid
-      reply = chooseMove(! side, alpha, beta);
-      undo move m; // Restores "this" Grid
-      if (side == COMPUTER && reply.score > myBest.score) {
-        myBest.move = m;
-        myBest.score = reply.score;
-        alpha = reply.score;
-      } else if (side == HUMAN && reply.score < myBest.score) {
-        myBest.move = m;
-        myBest.score = reply.score;
-        beta = reply.score;
-      }
-      if (alpha >= beta) { return myBest; }
-    }
-    return myBest;
-  }
-=end
