2439ecd55cd7334575a960158021d0415b49d98e
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2014-10-02 14:25:11 -0700

    Change database to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140916212302) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "boards", force: true do |t|
     t.text     "white"
     t.text     "black"
