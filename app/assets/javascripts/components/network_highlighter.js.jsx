bedc3a2b46b393fa1b07b60928ee5279fae3e7b8
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2014-10-14 16:05:00 -0700

    Draw lines for complete networks

--- app/assets/javascripts/components/network_highlighter.js.jsx
+++ app/assets/javascripts/components/network_highlighter.js.jsx
@@ -15,22 +15,51 @@ var NetworkHighlighter = React.createClass({
       network = this.state.white_network[index];
     }
     return function() {
-      console.log(network);
       var winners = document.querySelectorAll("td.winner");
+      //Erase previous highlighted networks
       for (var i=0; i < winners.length; i++) {
-        console.log(winners.length);
         var square = winners[i];
         square.className = square.className.replace(/\bwinner\b/, '');
 
+      }
+      //Erase all lines
+      var lines = document.querySelectorAll("div.line");
+      for (var i=0; i < lines.length; i++) {
+        lines[i].parentNode.removeChild(lines[i]);
       }
       for (var i=0; i < network.length; i++) {
         var coordinate = network[i];
         var table = document.getElementsByClassName("board_table")[0];
         var square = table.getElementsByClassName("board_square" + coordinate)[0];
+        if ( i < network.length - 1) {
+          var line = this.createLine(table, coordinate, network[i+1]);
+          square.appendChild(line);
+        }
         square.className+= " winner";
       }
       return false;
-    }
+    }.bind(this);
+  },
+  createLine: function(table, point1, point2) {
+    console.log(point1, point2);
+    var square1 = table.getElementsByClassName("board_square" + point1)[0];
+    var square2 = table.getElementsByClassName("board_square" + point2)[0];
+    var x1 = square1.offsetLeft
+    var y1 = square1.offsetTop
+    var x2 = square2.offsetLeft
+    var y2 = square2.offsetTop
+    //Calculate length and angle of line so it renders correctly
+    var length = Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+    var angle  = Math.atan2(y2 - y1, x2 - x1) * 180 / Math.PI;
+    var transform = 'rotate('+angle+'deg)';
+
+    var line = document.createElement("div");
+    line.className = "line"
+
+    //Set CSS properties of line
+    line.style.width = length + "px";
+    line.style.transform = transform;
+    return line;
   },
   render: function() {
     var black_links = [],
